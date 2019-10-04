34728809402fe92cb2f41f914581260d47f0bdfc
Purvik Rana <purvk.rana@thesunflowerlab.com>
2019-10-04 12:52:10 +0530

    Signout Option Added

--- app/src/main/java/com/student/usermanagementdemo/activities/MainActivity.java
+++ app/src/main/java/com/student/usermanagementdemo/activities/MainActivity.java
@@ -2,6 +2,9 @@ package com.student.usermanagementdemo.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -29,10 +32,28 @@ public class MainActivity extends AppCompatActivity {
         tvSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_home_menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out:
                 firebaseAuth.signOut();
                 startActivity(new Intent(MainActivity.this, LoginActivity.class)
                         .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-            }
-        });
+                return true;
+            default:
+                return true;
+        }
     }
 }
